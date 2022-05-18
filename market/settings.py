61104a0007e1657e05f44847be3791e0d9cd7c4b
Abdulrahman Kabil <63946776+Abdulrahman-Kabil-developer@users.noreply.github.com>
2022-05-18 10:48:19 +0200

    add new feature send password by email and edit forms

--- market/settings.py
+++ market/settings.py
@@ -146,3 +146,11 @@ AUTHENTICATION_BACKENDS = ['core.backends.NormalUserBackend','core.backends.Supe
 
 RECAPTCHA_PUBLIC_KEY = '6LdhO_gfAAAAAHK4fNUnuX4rzMwryaP47AZWjATp'
 RECAPTCHA_PRIVATE_KEY = '6LdhO_gfAAAAAFyeF0-k1B6pmlpmukr2plkNe2ru'
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "souqproject67@gmail.com"
+EMAIL_HOST_PASSWORD = "riopjlezcboobnxd"
--- sellers/views.py
+++ sellers/views.py
@@ -2,6 +2,9 @@ import imp
 from django.shortcuts import render, redirect
 from core.models import Authorization
 from sellers.models import Seller
+from users.views import generateRondamPassword
+from django.core.mail import EmailMultiAlternatives
+from market.settings import EMAIL_HOST_USER
 
 # Create your views here.
    
@@ -14,11 +17,20 @@ def signupBusiness (request):
                 email= request.POST.get('email')
                 taxNumber = request.POST.get('Com-Num')
                 companyDescription = request.POST.get('company_description')
-                password= request.POST.get('password')
+                password= generateRondamPassword()
                 type=Authorization.objects.get(auth_name="seller")
                 data=Seller(companyName=companyName, email=email, taxNumber=taxNumber, companyDiscription=companyDescription, password=password, sellerPhoto=request.FILES['fileName'],userAuth=type)
                 data.save()
-                return redirect('login')
+                 #send email to company
+                subject = 'Welcome '+companyName+' in Souq Market'
+                message = 'Hope you are enjoying your account with us. Your password is: ' + password + " you can login to your account and reset your password in your profile page thank you. Souq Market admins team."
+                htmlMessage="<h1>Welcome "+companyName+" in Souq Market.</h1><br><h2><p>Hope you are enjoying your account with us.</p> Your password is:</h2><h1> " + password + "</h1> <h2><p>you can login to your account and reset your password in your profile page thank you.</p></h2> <h4>Souq Market admins team.</h4>"
+                to = str(email)
+                mail=EmailMultiAlternatives(subject,message,EMAIL_HOST_USER, [to])
+                mail.attach_alternative(htmlMessage, "text/html")
+                mail.send()
+                return render(request,'core/login.html',{'message': 'You have successfully registered. Please check your email to get your password.'})
+
         else:
             return render(request, 'sellers/business_signup.html', {'error': 'Please check the recaptcha'})
     return render(request, 'sellers/business_signup.html',{})
