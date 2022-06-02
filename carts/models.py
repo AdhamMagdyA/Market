f12fa1ca2801a3f5800796a573b2f5f0b97cb934
Abdulrahman Kabil <63946776+Abdulrahman-Kabil-developer@users.noreply.github.com>
2022-06-02 03:45:26 +0200

    add cart feature with all functionality

--- carts/models.py
+++ carts/models.py
@@ -6,6 +6,10 @@ from products.models import Product
 # Create your models here.
 
 class Cart(models.Model):
+    
     cartName=models.CharField(max_length=30, default="My Cart")
     cartProducts=models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.cartName
 
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from users.models import User
+from products.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def get(request):
-    return render(request, 'carts/cart.html')
+def index(request):
+    id1=request.user.user_id
+    cart = User.objects.get(user_id=id1).userCart
+    data=cart.cartProducts.all()
+    return render(request, 'carts/cart.html', {'data': data})
+
+#@login_required (login_url='login')
+def addToCart(request,id):
+    try:
+        userFound=User.objects.get(first_name=request.user)
+    except:
+        userFound=None
+    print("user= "+str(request.user))
+    if (userFound!=None):
+        id1=request.user.user_id
+        cart = User.objects.get(user_id=id1).userCart
+        product = Product.objects.get(id=id)
+        cart.cartProducts.add(product)
+        cart.save()
+        return redirect('cart')
+    else:
+        return redirect('login')
+    
+    
+def removeFromCart(request,id):
+    try:
+        userFound=User.objects.get(first_name=request.user)
+    except:
+        userFound=None
+    print("user= "+str(request.user))
+    if (userFound!=None):
+        id1=request.user.user_id
+        cart = User.objects.get(user_id=id1).userCart
+        product = Product.objects.get(id=id)
+        cart.cartProducts.remove(product)
+        cart.save()
+        return redirect('cart')
+    else:
+        return redirect('login')
+
+    
